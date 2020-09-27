17b213772f4c147d9a74de22c3c2141b8205b42e
rita <kajirita2002@gmail.com>
2020-09-27 16:11:28 +0900

    ライフサイクルの流れを理解しよう

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -6,8 +6,22 @@ class Blog extends React.Component {
         super(props)
         this.state = {
             isPublished: false,
-            order: 1
+            order: 1,
+            count: 0
         }
+    }
+
+    componentDidMount() {
+        document.getElementById("counter").addEventListener('click', this.countUp)
+    }
+
+    componentDidUpdate() {
+        if (this.state.count >= 10) {
+            this.setState({count: 0})
+        }
+    }
+    componentWillUnmount() {
+        document.getElementById("counter").removeEventListener('click', this.countUp)
     }
 
     togglePublished = () => {
@@ -19,10 +33,23 @@ class Blog extends React.Component {
         })
     };
 
+    countUp = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
     render() {
         return (
             <>
-                <Article title="リタの記事" isPublished={this.state.isPublished} order={this.state.order} toggle={() => this.togglePublished}/>
+                <Article
+                    title="リタの記事"
+                    isPublished={this.state.isPublished}
+                    order={this.state.order}
+                    toggle={() => this.togglePublished}
+                    count={this.state.count}
+                    />
+
             </>
         )
     }
