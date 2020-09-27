2947e9d3361c2fca1883a9a21c172561557d115d
rita <kajirita2002@gmail.com>
2020-09-27 23:41:23 +0900

    チュートリアル終了

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,58 +1,42 @@
 import React from 'react';
 import Article from './Article';
 
-class Blog extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isPublished: false,
-            order: 1,
-            count: 0
-        }
-    }
 
-    componentDidMount() {
-        document.getElementById("counter").addEventListener('click', this.countUp)
-    }
+const Blog =() => {
 
-    componentDidUpdate() {
-        if (this.state.count >= 10) {
-            this.setState({count: 0})
-        }
-    }
-    componentWillUnmount() {
-        document.getElementById("counter").removeEventListener('click', this.countUp)
-    }
 
-    togglePublished = () => {
-        this.setState({
-            isPublished: !this.state.isPublished
-        })
-        this.setState({
-            order: 2
-        })
-    };
 
-    countUp = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
+    // componentDidMount() {
+    //     document.getElementById("counter").addEventListener('click', this.countUp)
+    // }
 
-    render() {
-        return (
-            <>
-                <Article
-                    title="リタの記事"
-                    isPublished={this.state.isPublished}
-                    order={this.state.order}
-                    toggle={() => this.togglePublished}
-                    count={this.state.count}
-                    />
+    // componentDidUpdate() {
+    //     if (this.state.count >= 10) {
+    //         this.setState({count: 0})
+    //     }
+    // }
+    // componentWillUnmount() {
+    //     document.getElementById("counter").removeEventListener('click', this.countUp)
+    // }
+
+
+
+    // countUp = () => {
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     })
+    // }
+
+
+    return (
+        <>
+            <Article title="リタの記事"
+                // count={this.state.count}
+            />
 
             </>
         )
-    }
+
 }
 
 export default Blog
